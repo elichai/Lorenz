52a19ab07d63b4cc85a42b3a7fc2f45dd4871453
Elichai Turkel <elichai.turkel@gmail.com>
2019-06-04 16:49:47 -0400

    Fixed encryption

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,8 +1,8 @@
+use crate::Error;
 use ring::aead::{
-    self, seal_in_place, Aad, Nonce, SealingKey, AES_256_GCM, CHACHA20_POLY1305,
+    self, seal_in_place, Aad, Nonce, SealingKey, AES_256_GCM, CHACHA20_POLY1305, NONCE_LEN,
 };
 use ring::rand::{SecureRandom, SystemRandom};
-use crate::Error;
 
 pub enum Scheme {
     AES256GCM,
@@ -14,22 +14,32 @@ pub fn encrypt(key: &[u8], mut data: Vec<u8>, scheme: Scheme) -> Result<Vec<u8>,
     if key.len() != algorithm.key_len() {
         return Err(Error::BadKeyLength);
     }
+    println!("key: {:?}", key);
     let key = SealingKey::new(algorithm, key).map_err(Error::encryption)?;
-    let nonce = get_random_nonce()?;
+    let (nonce, raw_nonce) = get_random_nonce()?;
 
-    data.extend(vec![0u8; algorithm.tag_len()]);
-    let s = seal_in_place(&key, nonce, Aad::empty(), &mut data, 0).map_err(Error::encryption)?;
+    data.extend(vec![0u8; algorithm.tag_len()+5]);
+    let s = seal_in_place(
+        &key,
+        nonce,
+        Aad::empty(),
+        &mut data,
+        algorithm.tag_len()
+    )
+    .map_err(Error::encryption)?;
 
     debug_assert_eq!(s, data.len());
+
+    data.extend(&raw_nonce);
 
     Ok(data)
 }
 
-fn get_random_nonce() -> Result<Nonce, Error> {
+fn get_random_nonce() -> Result<(Nonce, [u8; 12]), Error> {
     let rand_gen = SystemRandom::new();
-    let mut nonce = [0u8; 12];
+    let mut nonce = [0u8; NONCE_LEN];
     rand_gen.fill(&mut nonce).map_err(Error::io)?;
-    Ok(Nonce::assume_unique_for_key(nonce))
+    Ok((Nonce::assume_unique_for_key(nonce), nonce))
 }
 
 fn scheme_to_algorithm(scheme: Scheme) -> &'static aead::Algorithm {
